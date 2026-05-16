6657cf340910a52620a88d09589f7428517c456d
Marlon <marlonmoorer@outlook.com>
2026-05-16 12:46:39 -0400

    migration folder

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,7 @@ WORKDIR /app
 
 COPY --from=builder /app/build ./build
 COPY --from=builder /app/package.json ./package.json
+COPY --from=builder /app/drizzle ./drizzle
 
 EXPOSE 3000
 ENV NODE_ENV=production
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,8 +4,6 @@ import * as schema from "./schema";
 import { env } from "$env/dynamic/private";
 import { building } from "$app/environment";
 
-console.log({ building });
-console.log({ env, nodeEnv: process.env });
 // if (!env.DATABASE_URL && !building) throw new Error("DATABASE_URL is not set");
 
 const client = postgres(env.DATABASE_URL);
